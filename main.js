ebcf58339f96d499fe7cdab6b6dff012926b369e
Romain Metayer <33374185+metayerromain@users.noreply.github.com>
2018-06-21 15:22:09 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var container = document.querySelector('.container');
+var sections = document.querySelectorAll('section');
+var screenWidth = window.innerWidth;
+var decalage;
+var posX;
+
+for (var i = 0; i < sections.length; i++) {
+  container.addEventListener("touchmove", function(e){
+    posX = e.touches[0].clientX;
+    console.log(screenWidth - posX);
+    decalage = screenWidth - posX;
+    sections[0].style.transform = "translateX(" + decalage + "px)";
+    //sections[1].style.transform = "translateX(" + decalage + "px)";
+  });
+  container.addEventListener("touchend", function(){
+    if (posX < screenWidth/2) {
+      sections[1].style.transform = "translateX(0)";
+    }
+  })
+}
