93bdeb6cc0605488c2a500841fe69a8b0a4cc72a
Romain Metayer <33374185+metayerromain@users.noreply.github.com>
2018-06-22 15:14:42 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-var container = document.querySelector('.container');
-var sections = document.querySelectorAll('section');
-var screenWidth = window.innerWidth;
-var decalage;
-var posX;
+document.addEventListener('DOMContentLoaded', function(){
 
-for (var i = 0; i < sections.length; i++) {
-  container.addEventListener("touchmove", function(e){
-    posX = e.touches[0].clientX;
-    console.log(screenWidth - posX);
-    decalage = screenWidth - posX;
-    sections[0].style.transform = "translateX(" + decalage + "px)";
-    //sections[1].style.transform = "translateX(" + decalage + "px)";
+  window.mySwipe = new Swipe(document.getElementById('slider'), {
+    startSlide: 0,
+    speed: 400,
+    auto: 3000,
+    draggable: true,
+    continuous: false,
+    disableScroll: true,
+    stopPropagation: true,
+    callback: function(index, elem, dir) {},
+    transitionEnd: function(index, elem) {}
   });
-  container.addEventListener("touchend", function(){
-    console.log(posX, screenWidth/2)
-    if (posX < screenWidth/2) {
-      sections[0].style.transform = "translateX(0)";
-      sections[1].style.transform = "translateX(-100vw)";
-    }
-  })
-}
+
+
+
+  // включить видимость блока (по умолчанию скрыт)
+  document.getElementById('slider').classList.add('swipe--enable');
+
+});
