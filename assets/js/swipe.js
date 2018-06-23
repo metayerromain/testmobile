b928f46ed7515fdc61dbee5810854c8c9ee26de5
Romain Metayer <33374185+metayerromain@users.noreply.github.com>
2018-06-24 01:50:15 +0200

    Add files via upload

--- assets/js/swipe.js
+++ assets/js/swipe.js
@@ -3,6 +3,8 @@ var p2 = document.getElementById('client2');
 var p3 = document.getElementById('client3');
 
 var line = document.querySelector(".header__line-inner");
+var tabs = document.querySelectorAll(".header__tabs-item");
+var pages = document.querySelectorAll(".page");
 
 var startingX;
 
@@ -24,7 +26,7 @@ function p1move(e){
 
 function p1end(e){
   var change = startingX - e.changedTouches[0].clientX;
-  var half = screen.width / 3;
+  var half = screen.width / 5;
   if (change < half) {
     p1.style.left = 0;
     p2.style.left = '100%';
@@ -62,7 +64,7 @@ function p2move(e){
 
 function p2end(e){
   var change = e.changedTouches[0].clientX - startingX;
-  var half = screen.width / 3;
+  var half = screen.width / 5;
   if (change < half) {
     p1.style.left = '-200%';
     p1.style.display = "none";
@@ -101,7 +103,7 @@ function p3move(e){
 
 function p3end(e){
   var change = e.changedTouches[0].clientX - startingX;
-  var half = screen.width / 3;
+  var half = screen.width / 5;
   if (change < half) {
     p2.style.left = '-100%';
     p2.style.display = "none";
@@ -111,5 +113,41 @@ function p3end(e){
     p3.style.display = "none";
     line.classList.remove("line3");
     line.classList.add("line2");
+  }
+}
+
+function clickTabs(e){
+  var num = e.target.className.split(" ")[1];
+  console.log(num);
+  for (var i = 0; i < tabs.length; i++) {
+    pages[i].style.display = "none";
+  }
+  if (num == 0) {
+    p3.style.left = "200%";
+    p2.style.left = "100%";
+    p1.style.left = "0";
+    pages[num].style.display = "block";
+    var activeLine = line.className.split(" ")[1];
+    line.classList.remove(activeLine);
+    line.classList.add("line1");
+  }
+  else if (num == 1) {
+    p3.style.left = "100%";
+    p2.style.left = "0";
+    p1.style.left = "-100%";
+    pages[num].style.display = "block";
+    line.classList.add("line2");
+    var activeLine = line.className.split(" ")[1];
+    line.classList.remove(activeLine);
+    line.classList.add("line2");
+  } else {
+    p3.style.left = "0";
+    p2.style.left = "-100%";
+    p1.style.left = "-200%";
+    pages[num].style.display = "block";
+    line.classList.add("line3");
+    var activeLine = line.className.split(" ")[1];
+    line.classList.remove(activeLine);
+    line.classList.add("line3");
   }
 }
